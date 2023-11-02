5130af18694d4d6abc20eb9d88d2dd08011bdf2d
ikram9820 <ikram9820khan@gmail.com>
2023-11-02 14:27:00 +0500

    adding issue detail page

---
+++ app/issues/[id]/page.tsx
@@ -0,0 +1,32 @@
+import { IssueStatusBadge } from "@/app/components";
+import prisma from "@/prisma/client";
+import { Heading, Text, Flex, Card } from "@radix-ui/themes";
+import { notFound } from "next/navigation";
+import ReactMarkdown from "react-markdown";
+
+import React from "react";
+
+interface Props {
+  params: { id: string };
+}
+
+const IssueDetailPage = async ({ params }: Props) => {
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+  if (!issue) notFound();
+  return (
+    <>
+      <Heading>{issue.title}</Heading>
+      <Flex className="space-x-3" my="2">
+        <IssueStatusBadge status={issue.status} />
+        <Text>{issue.createdAt.toDateString()}</Text>
+      </Flex>
+      <Card className="prose max-w-full" mt="4">
+        <ReactMarkdown>{issue.description}</ReactMarkdown>
+      </Card>
+    </>
+  );
+};
+
+export default IssueDetailPage;
