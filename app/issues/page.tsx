1a16f16aaf9a88f4132b494a469ca0527925f2fc
ikram9820 <ikram9820khan@gmail.com>
2023-11-02 14:05:30 +0500

    adding issues and skeleton for issues

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,14 +1,63 @@
 import React from "react";
-import { Button } from "@radix-ui/themes";
-import Link from "next/link";
-const IssuesPage = () => {
+import { Table } from "@radix-ui/themes";
+import prisma from "@/prisma/client";
+import { Issue } from "@prisma/client";
+import { IssueStatusBadge, Link } from "../components";
+import IssueActions from "./IssueActions";
+const IssuesPage = async () => {
+  const issues = await prisma.issue.findMany();
   return (
     <div>
-      <Button>
-        <Link href="/issues/new">New Issue</Link>
-      </Button>
+      <IssueActions />
+      <Table.Root variant="surface" className="max-w-xl">
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Status</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Date</Table.ColumnHeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {issues.map((issue) => (
+            <Table.Row key={issue.id}>
+              <Table.Cell>
+                <Link href={`/issues/${issue.id}`} children={issue.title} />
+                <div className="block md:hidden">
+                  <IssueStatusBadge status={issue.status} />
+                </div>
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                <IssueStatusBadge status={issue.status} />
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                {issue.createdAt.toDateString()}
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
     </div>
   );
 };
 
 export default IssuesPage;
+
+const columns: {
+  label: string;
+  value: keyof Issue;
+  className?: string;
+}[] = [
+  { label: "Issue", value: "title" },
+  {
+    label: "Status",
+    value: "status",
+    className: "hidden md:table-cell",
+  },
+  {
+    label: "Created",
+    value: "createdAt",
+    className: "hidden md:table-cell",
+  },
+];
+
+export const columnNames = columns.map((column) => column.value);
