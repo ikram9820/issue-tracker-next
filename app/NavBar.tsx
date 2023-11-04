cc2498c10b6fb70cbe0b594cd96ab18fccf3d3a2
ikram9820 <ikram9820khan@gmail.com>
2023-11-04 16:57:25 +0500

    refactoring navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -13,14 +13,9 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
 import { AiFillBug } from "react-icons/ai";
+import { Skeleton } from "./components";
 
-const links = [
-  { href: "/", label: "Dashboard" },
-  { href: "/issues/list", label: "Issues" },
-];
 const NavBar = () => {
-  const pathname = usePathname();
-  const { status, data: session } = useSession();
   return (
     <nav className="border-b py-3 px-5 mb-5">
       <Container>
@@ -29,52 +24,70 @@ const NavBar = () => {
             <Link className="text-red-400 text-2xl" href="/">
               <AiFillBug />
             </Link>
-            <ul className="flex space-x-6 ">
-              {links.map((link) => (
-                <li key={link.href}>
-                  <Link
-                    className={classNames({
-                      "text-zinc-900": link.href === pathname,
-                      "text-zinc-500": link.href !== pathname,
-                      "hover:text-zinc-800 transition-colors": true,
-                    })}
-                    href={link.href}
-                  >
-                    {link.label}
-                  </Link>
-                </li>
-              ))}
-            </ul>
+            <NavLinks />
           </Flex>
-          <Box>
-            {status === "authenticated" && (
-              <DropdownMenu.Root>
-                <DropdownMenu.Trigger>
-                  <Avatar
-                    src={session.user?.image!}
-                    fallback="?"
-                    size="2"
-                    radius="full"
-                    className="cursor-pointer"
-                  />
-                </DropdownMenu.Trigger>
-                <DropdownMenu.Content>
-                  <DropdownMenu.Label>
-                    <Text size="2">{session.user?.email}</Text>
-                  </DropdownMenu.Label>
-                  <DropdownMenu.Item>
-                    <Link href="/api/auth/signout"> Logout</Link>
-                  </DropdownMenu.Item>
-                </DropdownMenu.Content>
-              </DropdownMenu.Root>
-            )}
-            {status === "unauthenticated" && (
-              <Link href="/api/auth/signin"> Login</Link>
-            )}
-          </Box>
+          <AuthStatus />
         </Flex>
       </Container>
     </nav>
+  );
+};
+
+const NavLinks = () => {
+  const links = [
+    { href: "/", label: "Dashboard" },
+    { href: "/issues/list", label: "Issues" },
+  ];
+  const pathname = usePathname();
+
+  return (
+    <ul className="flex space-x-6 ">
+      {links.map((link) => (
+        <li key={link.href}>
+          <Link
+            className={classNames({
+              "nav-link": true,
+              "!text-zinc-900": link.href === pathname,
+            })}
+            href={link.href}
+          >
+            {link.label}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+const AuthStatus = () => {
+  const { status, data: session } = useSession();
+  if (status === "loading") return <Skeleton width='3rem' />;
+  if (status === "unauthenticated")
+    return <Link className="nav-link" href="/api/auth/signin"> Login</Link>;
+
+  return (
+    <Box>
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger>
+          <Avatar
+            src={session!.user?.image!}
+            fallback="?"
+            size="2"
+            radius="full"
+            className="cursor-pointer"
+            referrerPolicy="no-referrer"
+          />
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Content>
+          <DropdownMenu.Label>
+            <Text size="2">{session!.user?.email}</Text>
+          </DropdownMenu.Label>
+          <DropdownMenu.Item>
+            <Link href="/api/auth/signout"> Logout</Link>
+          </DropdownMenu.Item>
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
+    </Box>
   );
 };
 
